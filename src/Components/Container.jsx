90cf1b5017b7ed542a25fa3c065800bbed7d2e06
Denis Holc <holc.denis7@gmail.com>
2023-11-16 21:12:41 +0100

    New changes to all tasks

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -24,7 +24,7 @@ const MainContainer = () => {
               {/* <TasksSuggestions /> */}
               {/* <DigitalClock /> */}
               <Alltasks />
-              <MyTasks />
+              {/* <MyTasks /> */}
               {/* <HomepageTask/> */}
             
               </div>
