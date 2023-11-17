ac2cd9d481082ce17d35a6f3eae4f79052c9b7a0
Denis Holc <holc.denis7@gmail.com>
2023-11-17 22:38:35 +0100

    New changes

--- src/Components/CreateTask.jsx
+++ src/Components/CreateTask.jsx
@@ -1,30 +1,80 @@
-import React from 'react'
+
+
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAdd, faUser, faTasksAlt, faCalendarAlt, faCheck, faDeleteLeft, faEdit, faEyeDropper, faHome, faHomeAlt, faTrash, faSuitcase, faPersonChalkboard, faJetFighter, faSchool, faExclamationTriangle, faMinus, faArrowCircleDown, faCircle, faSmile } from '@fortawesome/free-solid-svg-icons';
-import sentMessage from "../assets/send-mess.png"
+import {
+  faAdd, faSuitcase, faSchool, faJetFighter,
+  faExclamationTriangle, faMinus, faArrowCircleDown, faSmile
+} from '@fortawesome/free-solid-svg-icons';
+import sentMessage from "../assets/send-mess.png";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function CreateTask() {
+  const [task, setTask] = useState("");
+  const [dueDate, setDueDate] = useState(new Date());
+  const [isCalendarVisible, setIsCalendarVisible] = useState(false);
 
-    const formContainer = (e) => {
-        e.preventDefault()
+  const handleTaskChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleDateChange = (date) => {
+    setDueDate(date);
+    setIsCalendarVisible(false); 
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(task.trim() === "") {
+        alert("Please type something!") && setIsCalendarVisible(false)
+    
+    
+
+    return;
     }
+    console.log('Task:', task, 'Due Date:', dueDate.toLocaleDateString());
+    
+    setTask("");
+    setDueDate(new Date());
+    setIsCalendarVisible(false);
+  };
 
   return (
     <div className='create-task-container d-flex flex-column'>
-        <h4>Task name</h4> 
-      <div className='glassmorphic-search-bar-cat' >
-        <form onSubmit={formContainer}>
-            <input type='text' placeholder='Add new task....'></input>
-            <button className='quick-add-button-add' type='submit'>
-                <img src={sentMessage} alt='sent-message' style={{ width: "30px"}}/>
-            </button>
+      <h4>Task name</h4>
+      <div className='glassmorphic-search-bar-cat'>
+        <form onSubmit={handleSubmit}>
+          <input 
+            onChange={handleTaskChange} 
+            onFocus={() => setIsCalendarVisible(true)} 
+            value={task} 
+            type='text' 
+            placeholder='Add new task....'
+          />
+          {/* <input 
+            type="text" 
+            value={dueDate.toLocaleDateString()}  
+            readOnly 
+            onClick={() => setIsCalendarVisible(!isCalendarVisible)} 
+            placeholder='Due date'
+          /> */}
+          {isCalendarVisible && (
+            <Calendar 
+              onChange={handleDateChange} 
+              value={dueDate} 
+            />
+          )}
+           <button className='quick-add-button-add' type='submit'>
+            <img src={sentMessage} alt='sent-message' style={{ width: "30px"}}/>
+          </button>
         </form>
       </div>
 
-      <h5 className='category-text mt-4'>Categories</h5> 
-      <div className='category-container'>
+      <h5 className='category-text mt-3'>Categories</h5> 
+      <div className={!isCalendarVisible ?  'category-container' : "category-container-active-calendar"}>
         <div className="category-body-container">
-            <div className='category-separator mt-2 d-flex justify-content-around'>
+            <div className='category-separator mt-1 d-flex justify-content-around'>
 
                     <div className="icon-border-cat">
                         <FontAwesomeIcon  className="test-fa-cat" icon={faSuitcase} size="2xl"/>
@@ -32,12 +82,12 @@ export default function CreateTask() {
                       </div>
                       <div className="icon-border-cat">
                         <FontAwesomeIcon  className="test-fa-cat" icon={faSchool} size="2xl"/>
-                        <div className="icon-text">SCHOOL</div>
+                        <div className="icon-text-school">SCHOOL</div>
                       </div>
     
                       <div className="icon-border-cat">
                         <FontAwesomeIcon  className="test-fa-cat" icon={faJetFighter} size="2xl"/>
-                        <div className="icon-text">HOLIDAY</div>
+                        <div className="icon-text-holiday">HOLIDAY</div>
                       </div>
                       <div className="icon-border-cat-add">
                         <FontAwesomeIcon  className="test-fa-cat-add" icon={faAdd} size="2xl"/>
@@ -55,8 +105,8 @@ export default function CreateTask() {
             </div>       
         </div>
 
-        <h5 className='category-text mt-5'>Urgency</h5> 
-      <div className='category-container'>
+        <h5 className='category-text'>Urgency</h5> 
+      <div className={!isCalendarVisible ?  'category-container' : "category-container-active-calendar"}>
         <div className="category-body-container">
             <div className='category-separator mt-2 d-flex justify-content-around'>
 
@@ -66,12 +116,12 @@ export default function CreateTask() {
                       </div>
                       <div className="icon-border-cat-medium">
                         <FontAwesomeIcon  className="test-fa-cat-minus" icon={faMinus} size="2xl"/>
-                        <div className="icon-text">MEDIUM</div>
+                        <div className="icon-text-medium">MEDIUM</div>
                       </div>
     
                       <div className="icon-border-cat-low">
                         <FontAwesomeIcon  className="test-fa-cat-low" icon={faArrowCircleDown} size="2xl"/>
-                        <div className="icon-text">LOW</div>
+                        <div className="icon-text-low">LOW</div>
                       </div>
                       <div className="icon-border-cat-add">
                         <FontAwesomeIcon  className="test-fa-cat-add" icon={faSmile} size="2xl"/>
